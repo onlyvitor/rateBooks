700fda493bf9b6467841632e76ba2ef38efa863b
vito <vitorgn1franca@protonmail.com>
2026-03-19 18:37:15 -0300

    refactor: refactor the auth guard

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,18 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly jwtService: JwtService) { }
 
-    canActivate(context: ExecutionContext): boolean {
+    async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
-        const token = request.headers.authorization;
+        const token = this.extractTokenFromHeader(request);
         if (!token) {
             throw new UnauthorizedException('No token provided');
         }
-        const decodedToken = this.jwtService.verify(token);
+        const decodedToken = await this.jwtService.verifyAsync(token);
         request.user = decodedToken;
         return true;
+    }
+
+    private extractTokenFromHeader(request: Request) {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
     }
 }
